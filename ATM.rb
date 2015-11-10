8be36898048e690cc526893f0af04cf12fafb836
Bilal Reza <rezasb@s.dcsdk12.org>
2015-11-10 10:21:47 -0700

    Make deposit

--- ATM.rb
+++ ATM.rb
@@ -1,6 +1,6 @@
 class ATM
-	attr_accessor :name, :pin, :balance, :current_name, :current_pin
-	attr_reader :name, :pin, :file, :balance, :current_name, :current_pin
+	attr_accessor :name, :pin, :balance, :current_name, :current_pin, :deposit
+	attr_reader :name, :pin, :file, :balance, :current_name, :current_pin, :deposit
 
 	def initialize
 		@name = name
@@ -8,15 +8,14 @@ class ATM
 		@balance = balance
 		@current_name = current_name
 		@current_pin = current_pin
-
+		@deposit = deposit
 		end
 
 
 	def authorize(name, pin, users)
 			for i in 0..(users.length - 1)
 				if users[i].pin = @current_pin
-					print "Enter 1 (Deposit), 2 (Withdraw), 3 (Balance), or 4 (Quit): "
-  				command = gets.chomp
+					prompt
   			else
   				print "That pin does not match"
 				end
@@ -28,5 +27,19 @@ class ATM
 		@current_name = gets.chomp
 		puts "Please enter your pin:"
 		@current_pin = gets.chomp
+	end
+
+	def prompt
+		print "Enter 1 (Deposit), 2 (Withdraw), 3 (Balance), or 4 (Quit): "
+  				command = gets.chomp
+  	if command == 1
+  		make_deposit
+  	end
+
+	end
+
+	def make_deposit
+		puts "How much would you like to deposit? "
+		@deposit = gets.chomp
 	end
 end
