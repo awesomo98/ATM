430ca3fe9fe9ddeef2e77d42b1c84f441f55ac67
Bilal Reza <rezasb@s.dcsdk12.org>
2015-11-11 10:12:19 -0700

    chomp

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ class Main
 	file = File.open("Accounts.csv")
 		file.each_line do |line|
 			array = line.chop.split(',')
-			users.push(User.new(array[0], array[1].to_i, array[2].chop))
+			users.push(User.new(array[0], array[1].to_i, array[2].chomp))
 		end
 
 
